0b5b81693f8f76075981f56caf7f36dbb8f1819e
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2021-02-13 19:09:46 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bug-relative-urls-are-not-converted-to-absolute-urls-ssr';
+}
